e4ebdf07b780f0a2a944b6ab5dcbc7d6adae580c
popam <popa.mariusz@gmail.com>
2021-10-13 22:53:52 +0200

    class Manager

--- accountant.py
+++ accountant.py
@@ -7,6 +7,31 @@ mode = ALLOWED_MODE
 saldo = None # poczatkowe saldo
 store = {}  # MAGAZYN
 
+
+class Manager:
+    def __init__(self):
+        self.actions = {}
+
+    def assign(self, name):
+        def decorate(cb):
+            self.actions[name] = cb
+        return decorate
+
+    def execute(self, name):
+        if name not in self.actions:
+            print("Action not defined")
+        else:
+            self.actions[name](self)
+
+# manager = Manager()
+
+# @manager.assign("printyes")
+# def printer(manager):
+#     print("yes")
+#
+#
+# manager.execute("printyes")
+
 file = open('baza_danych.txt', 'r')
 for line in file.readlines():
     if 'saldo' in line:
